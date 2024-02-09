768bf9b5e2bd8a1effa3fd131b0d104b15da636c
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-09 09:15:01 +0100

    Create exercise.py

---
+++ QUOTIENT AND REMAINDER/exercise.py
@@ -0,0 +1,4 @@
+n = input("Enter the dividend (integer): ")
+m = input("Enter the divisor (integer): ")
+
+print(n + " by " + m + " gives a quotient of " + str(int(n) // int(m)) + " end remainder of "+ str(int(n) % int(m)))
