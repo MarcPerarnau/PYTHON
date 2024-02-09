3091bf62d6ef9add8295e577b429b1d7edae48fc
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-09 09:31:32 +0100

    Create exercise.py

---
+++ TOY STORE/exercise.py
@@ -0,0 +1,7 @@
+clown = 112
+doll = 75
+
+n_doll = int(input("Enter the number of dolls sold: "))
+n_clown = int(input("Enter the number of clowns sold: "))
+
+print("The total weight is: ", int((n_doll*doll)+(n_clown*clown)), " kg")
