cf33b7e139c76cf4e7cb7dc64eae765b76909908
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-14 12:26:19 +0100

    Create exercise.py

---
+++ TAX OFFICE/exercise.py
@@ -0,0 +1,8 @@
+years = int(input("Enter your age: "))
+
+income = float(input("Enter your income: "))
+
+if years >= 16 and income >= 1000:
+    print("The tax agency knocks on your door.")
+else:
+    print("This time you have been saved but you are being watched.")
