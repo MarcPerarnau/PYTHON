d7347a16333a956c6a441d0294431d23796e3759
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-14 11:55:48 +0100

    Create exercise.py

---
+++ DIVISION/exercise.py
@@ -0,0 +1,7 @@
+dividend = int(input("Enter a dividend: "))
+divider = int(input("Enter a divider: "))
+
+if divider == 0:
+    print("Error cannot be divided by 0")
+else:
+    print(dividend/divider)
