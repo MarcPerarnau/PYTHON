0d1cde364805972e65168cf5f5aab4f7ad2cbfbf
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-23 16:30:45 +0100

    Create exercise.py

---
+++ TWO-DIGIT FUNTION/exercise.py
@@ -0,0 +1,9 @@
+def number_max(number1, number2):
+    return max(number1, number2)
+
+number1 = int(input("Enter a first number: "))
+number2 = int(input("Enter a second number: "))
+
+result = number_max(number1, number2)
+
+print("The highest number is:", result)
